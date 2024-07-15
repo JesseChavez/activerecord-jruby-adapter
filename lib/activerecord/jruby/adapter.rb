0f25e6f9fc8d916bf2f86f7c83dcd7b303786d30
Jesse Chavez <jesse.chavez.r@gmail.com>
2024-07-16 07:00:19 +1000

    generate gem with bundler
     bundler gem activerecord-jruby-adapter --test=test-unit

---
+++ lib/activerecord/jruby/adapter.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "adapter/version"
+
+module Activerecord
+  module Jruby
+    module Adapter
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
